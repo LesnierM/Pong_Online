b20e78662f2c000641bd0ae32643f22c22b0c063
Lesnier <lesniermarrero@gmail.com>
2022-09-09 17:59:21 -0400

    Improved networkinput data
    -Changed byte field for sbyte which accepts negative values.

--- Assets/Scripts/NetworkInputData.cs
+++ Assets/Scripts/NetworkInputData.cs
@@ -5,23 +5,19 @@ using Fusion;
 public struct NetworkInputData : INetworkInput
 {
     //Input axies values
-    byte _horizontalAxyInput;
-    byte _verticalAxyInput;
+    sbyte _horizontalAxyInput;
+    sbyte _verticalAxyInput;
     /// <summary>
     /// Sets the input.
     /// </summary>
     public void SetInputs(Vector2 direction)
     {
         direction.Normalize();
-        _horizontalAxyInput = (byte)direction.x;
-        _verticalAxyInput = (byte)direction.y;
+        _horizontalAxyInput = (sbyte)direction.x;
+        _verticalAxyInput = (sbyte)direction.y;
     }
     /// <summary>
     /// Gets the input.
     /// </summary>
-    public Vector2 Input => new Vector2(getCoprrectValue(_horizontalAxyInput),getCoprrectValue(_verticalAxyInput));
-    /// <summary>
-    /// Converts byte to float due to bytes only suports from 0 to 255.
-    /// </summary>
-    float getCoprrectValue(byte value)=> value == 255 ? -1 :value==0?0:1;
+    public Vector2 Input => new Vector2(_horizontalAxyInput,_verticalAxyInput);
 }
