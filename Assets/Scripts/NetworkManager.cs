544451cda973471709dab01266164c124205e5c5
Lesnier <lesniermarrero@gmail.com>
2022-09-09 16:37:54 -0400

    Created network manager
    -Added create join session buttons.

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,18 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using System;
+using UnityEngine.SceneManagement;
 
-public class NetworkManager : MonoBehaviour
+public class NetworkManager : MonoBehaviour, INetworkRunnerCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] NetworkPrefabRef _playerprefab;
+    NetworkRunner _networkRunner;
 
-    // Update is called once per frame
-    void Update()
+    #region Mono
+    private void OnGUI()
     {
-        
+        if (_networkRunner == null)
+        {
+            if (GUI.Button(new Rect(0, 0, 200, 40), "Host"))
+            {
+                StartGame(GameMode.Host);
+            }
+            if (GUI.Button(new Rect(0, 40, 200, 40), "Join"))
+            {
+                StartGame(GameMode.Client);
+            }
+        }
     }
+    #endregion
+
+    #region Methods
+    async void StartGame(GameMode mode)
+    {
+        // Create the Fusion runner and let it know that we will be providing user input
+        _networkRunner = gameObject.AddComponent<NetworkRunner>();
+        _networkRunner.ProvideInput = true;
+
+        // Start or join (depends on gamemode) a session with a specific name
+        await _networkRunner.StartGame(new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = "TestRoom",
+            Scene = SceneManager.GetActiveScene().buildIndex,
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+    }
+    #endregion  
+
+    #region Callbacks
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+    }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+    }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+    }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+    }
+    public void OnDisconnectedFromServer(NetworkRunner runner)
+    {
+    }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+    }
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+    }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+    }
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        ;
+    }
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+    }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
+    {
+    }
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+    }
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+    }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+    }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+    }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+    }
+    #endregion
 }
